eb1cf45a7256f56fa6f23c859d7bfcb8d6e53127
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-02-22 00:27:14 -0700

    Correct data

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
-      list: resolve(__dirname, 'src/items-listing/index.html'),
-      details: resolve(__dirname, 'src/item-details/index.html')
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        list: resolve(__dirname, 'src/items-listing/index.html'),
+        details: resolve(__dirname, 'src/item-details/index.html')
+      },
+
     },
   },
 });
