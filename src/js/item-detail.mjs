626c909d6dca9793a61bd99bf755b3be520c047f
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-02-21 23:56:52 -0700

    Finish code

--- src/js/item-detail.mjs
+++ src/js/item-detail.mjs
@@ -5,19 +5,27 @@ import { GetParams, SetTitle } from './utils';
 
 const tableData = document.getElementById('table-values');
 const titleItem = document.getElementById('title-item');
+const informationHeader = document.getElementById('item-information');
 let category = '';
 let symbol = '';
 async function init() {
     category = GetParams('category');
     symbol = GetParams('symbol');
     const data = await GetSpecificDataItem(symbol, category);
-    console.log(data);
     TableForValues(tableData.tBodies[0], data.values);
     SetTitle(symbol, document)
     SetTitleItem();
+    RenderHeader(data);
     RegisterVisit(symbol, category);
 }
-
+function RenderHeader(data) {
+    informationHeader.innerHTML += `
+        ${data.meta.currency_quote != null && data.meta.currency_base != null ? `<h3>Base: <span class= 'subtitle'>${data.meta.currency_base}</span> Quote: <span class= 'subtitle'>${data.meta.currency_quote}</span></h3>` : ''}
+        ${data.meta.currency != null ? `<h3>Currency: <span class= 'subtitle'>${data.meta.currency}</span></h3>` : ''}
+        <p>Type: ${data.meta.type}</p>
+        
+    `;
+}
 function SetTitleItem() {
     titleItem.innerHTML = '';
     titleItem.innerHTML = symbol;
